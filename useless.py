fbcd580106e362c767b2770548ff81297a85a126
VilliamJ <villiammoltej@gmail.com>
2023-12-02 00:23:45 +0100

    wopo

--- useless.py
+++ useless.py
@@ -2,7 +2,6 @@ from psychopy import visual, core, event, data, gui
 import pandas as pd
 import os
 import random
-import colorsys
 
 # define dialogue box (important that this happens before you define the window)
 box = gui.Dlg(title="Sizes of Circles")
@@ -78,12 +77,8 @@ def show_circles():
     size_right = random.uniform(0.1, 0.15)
     saturation_right = random.uniform(0.7, 1)
 
-    # Convert RGB to HSV for logging
-    color_rgb_left = colorsys.hsv_to_rgb(hue_left, saturation_left, brightness_left)
-    color_rgb_right = colorsys.hsv_to_rgb(hue_right, saturation_right, brightness_right)
-
-    circle1 = visual.Circle(win, radius=size_left, fillColor=color_rgb_left, pos=(-0.5, 0))
-    circle2 = visual.Circle(win, radius=size_right, fillColor=color_rgb_right, pos=(0.5, 0))
+    circle1 = visual.Circle(win, radius=size_left, fillColor=(hue_left, saturation_left, brightness_left), pos=(-0.5, 0))
+    circle2 = visual.Circle(win, radius=size_right, fillColor=(hue_right, saturation_right, brightness_right), pos=(0.5, 0))
     fixation_cross = visual.TextStim(win, text='+', pos=(0, 0), color='white', height=0.05)
 
     circle1.draw()
@@ -98,8 +93,7 @@ def show_circles():
 
     return (
         brightness_left, size_left, hue_left, saturation_left,
-        brightness_right, size_right, hue_right, saturation_right,
-        color_rgb_left, color_rgb_right
+        brightness_right, size_right, hue_right, saturation_right
     )
 
 # preparing circles stimulus
@@ -107,8 +101,7 @@ for _ in range(10):
     # show stimulus circles
     (
         brightness_left, size_left, hue_left, saturation_left,
-        brightness_right, size_right, hue_right, saturation_right,
-        color_rgb_left, color_rgb_right
+        brightness_right, size_right, hue_right, saturation_right
     ) = show_circles()
 
     # start recording reaction time
@@ -133,14 +126,14 @@ for _ in range(10):
         'gender': GENDER,
         'brightness_left': round(brightness_left, 2),
         'size_left': round(size_left, 2),
-        'hue_left': round(hue_left,2),
+        'hue_left': round(hue_left, 2),
         'saturation_left': round(saturation_left, 2),
         'brightness_right': round(brightness_right, 2),
         'size_right': round(size_right, 4),
-        'hue_right': round(hue_right, 4),
+        'hue_right': round(hue_right, 2),
         'saturation_right': round(saturation_right, 2),
-        'color_left': color_rgb_left,
-        'color_right': color_rgb_right,
+        'color_left': (round(hue_left, 2), round(saturation_left, 2), round(brightness_left, 2)),
+        'color_right': (round(hue_right, 2), round(saturation_right, 2), round(brightness_right, 2)),
         'keypress': response,
         'correct_choice': correct_choice,
         'reaction_time': round(reaction_time, 2)
