89a14a2f2f3b998f084fe26c0b3bbac06f91ad6e
VilliamJ <villiammoltej@gmail.com>
2023-12-04 15:09:27 +0100

    final version

--- useless.py
+++ useless.py
@@ -2,6 +2,7 @@ from psychopy import visual, core, event, data, gui
 import pandas as pd
 import os
 import random
+import colorsys
 
 # define dialogue box (important that this happens before you define the window)
 box = gui.Dlg(title="Sizes of Circles")
@@ -18,7 +19,7 @@ elif box.Cancel:  # To cancel the experiment if the popup is closed
     core.quit()
 
 # define window
-win = visual.Window(fullscr=False, color="black")
+win = visual.Window(fullscr=True, color="black")
 
 # define stop watch
 stopwatch = core.Clock()
@@ -45,12 +46,13 @@ instruction = '''
 Welcome to this experiment!\n\n
 In a moment, two circles will flash at the same time on the screen. \n\n
 Your task is to select the circle you perceive as being the biggest. You do this by pressing either the left or right arrow key on your keyboard. \n\n\n
-If you understand these rules, please press any key to begin the experiment.
+Before starting please place your hand on the left and right arrow key, so you are ready to answer. If you understand these rules, please press any key to begin and the experiment will begin immediately.
 '''
 
 goodbye = '''
 The experiment is done. Thank you for your participation
 '''
+
 
 
 # function for showing text and waiting for a key press
@@ -63,22 +65,35 @@ def msg(txt):
 
 msg(instruction)
 
-# function for showing circles
 def show_circles():
-    # Randomize brightness, hue, and size for the left circle
-    brightness_left = random.uniform(0.5, 1)
-    hue_left = random.uniform(0, 1)
-    size_left = random.uniform(0.1, 0.15)
-    saturation_left = random.uniform(0.7, 1)
+    # Define hue values for red, green, and blue
+    red_hue = 1
+    green_hue = 1/3
+    blue_hue = 2/3
+    
+    sb_properties = [1, 0.8, 0.6, 0.4]
+    
+    size_properties =  [0.1, 0.105, 0.11025, 0.11576, 0.12155, 0.12763, 0.13401, 0.14071, 0.14775, 0.15513]
 
-    # Randomize brightness, hue, and size for the right circle
-    brightness_right = random.uniform(0.5, 1)
-    hue_right = random.uniform(0, 1)
-    size_right = random.uniform(0.1, 0.15)
-    saturation_right = random.uniform(0.7, 1)
+    # Randomly choose one of the three hues
+    hue_left = random.choice([red_hue, green_hue, blue_hue])
+    hue_right = random.choice([red_hue, green_hue, blue_hue])
 
-    circle1 = visual.Circle(win, radius=size_left, fillColor=(hue_left, saturation_left, brightness_left), pos=(-0.5, 0))
-    circle2 = visual.Circle(win, radius=size_right, fillColor=(hue_right, saturation_right, brightness_right), pos=(0.5, 0))
+    # Randomize other properties
+    brightness_left = random.choice(sb_properties)
+    size_left = random.choice(size_properties)
+    saturation_left = random.choice(sb_properties)
+
+    brightness_right = random.choice(sb_properties)
+    size_right = random.choice(size_properties)
+    saturation_right = random.choice(sb_properties)
+
+    # Convert RGB to HSV for logging
+    color_rgb_left = colorsys.hsv_to_rgb(hue_left, saturation_left, brightness_left)
+    color_rgb_right = colorsys.hsv_to_rgb(hue_right, saturation_right, brightness_right)
+
+    circle1 = visual.Circle(win, radius=size_left, fillColor=color_rgb_left, pos=(-0.5, 0))
+    circle2 = visual.Circle(win, radius=size_right, fillColor=color_rgb_right, pos=(0.5, 0))
     fixation_cross = visual.TextStim(win, text='+', pos=(0, 0), color='white', height=0.05)
 
     circle1.draw()
@@ -93,15 +108,17 @@ def show_circles():
 
     return (
         brightness_left, size_left, hue_left, saturation_left,
-        brightness_right, size_right, hue_right, saturation_right
+        brightness_right, size_right, hue_right, saturation_right,
+        color_rgb_left, color_rgb_right
     )
 
 # preparing circles stimulus
-for _ in range(10):
+for _ in range(50):
     # show stimulus circles
     (
         brightness_left, size_left, hue_left, saturation_left,
-        brightness_right, size_right, hue_right, saturation_right
+        brightness_right, size_right, hue_right, saturation_right,
+        color_rgb_left, color_rgb_right
     ) = show_circles()
 
     # start recording reaction time
@@ -125,7 +142,7 @@ for _ in range(10):
         'age': AGE,
         'gender': GENDER,
         'brightness_left': round(brightness_left, 2),
-        'size_left': round(size_left, 2),
+        'size_left': round(size_left, 4),
         'hue_left': round(hue_left, 2),
         'saturation_left': round(saturation_left, 2),
         'brightness_right': round(brightness_right, 2),
@@ -136,9 +153,8 @@ for _ in range(10):
         'color_right': (round(hue_right, 2), round(saturation_right, 2), round(brightness_right, 2)),
         'keypress': response,
         'correct_choice': correct_choice,
-        'reaction_time': round(reaction_time, 2)
+        'reaction_time': round(reaction_time, 4)
     }, ignore_index=True)
-    
 
     # display for 250 ms
     win.flip()
